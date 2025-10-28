fe1da27b2fa9b598600b331b915604cbc409b72f
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-10-28 12:14:39 -0700

    Added sg90 servo header

--- include/sg90_servo_motor.hpp
+++ include/sg90_servo_motor.hpp
@@ -1,10 +1,18 @@
 #ifndef SG90_SERVO_MOTOR_HPP
 #define SG90_SERVO_MOTOR_HPP
 
+#include <wiringPi.h>
+
 class SG90_Servo_Motor
 {
 public:
+  SG90_Servo_Motor(int pinNumber) : pinNumber_(pinNumber)
+  {
+    pinMode(pinNumber_, PWM_OUTPUT);
+  }
+  void SetAngle(int degrees);
 private:
+  int pinNumber_;
 };
 
 #endif // SG90_SERVO_MOTOR_HPP
