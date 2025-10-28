9c08a4204cf438f75abedf3beae6e89d42dc6339
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-10-28 12:14:18 -0700

    Added ultrasonic header

--- include/hcsr04_ultrasonic_sensor.hpp
+++ include/hcsr04_ultrasonic_sensor.hpp
@@ -1,10 +1,23 @@
 #ifndef HCSRO4_ULTRASONIC_SENSOR_HPP
 #define HCSR04_ULTRASONIC_SENSOR_HPP
 
+#include <wiringPi.h>
+
 class HCSR04_Ultrasonic_Sensor
 {
 public:
+  HCSR04_Ultrasonic_Sensor(int triggerPinNumber, int echoPinNumber) :
+    triggerPinNumber_(triggerPinNumber), echoPinNumber_(echoPinNumber)
+  {
+    pinMode(triggerPinNumber_, OUTPUT);
+    pinMode(echoPinNumber_, INPUT);
+  }
+  float getDistance_m();
 private:
+  int triggerPinNumber_;
+  int echoPinNumber_;
+
+  float getPulseDuration_s();
 };
 
 #endif // HCSRO4_ULTRASONIC_SENSOR_HPP
