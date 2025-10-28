fc58779bd02d9b80224c77b5002303cca707c22c
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-10-28 12:14:28 -0700

    Added raindrop sensor

--- include/raindrop_sensor.hpp
+++ include/raindrop_sensor.hpp
@@ -1,10 +1,19 @@
 #ifndef RAINDROP_SENSOR_HPP
 #define RAINDROP_SENSOR_HPP
 
+#include <wiringPi.h>
+
 class Raindrop_Sensor
 {
 public:
+  Raindrop_Sensor(int pinNumber) : pinNumber_(pinNumber) 
+  {
+    pinMode(pinNumber_, INPUT);
+  }
+  bool isThereWater();
+  
 private:
+  int pinNumber_;
 };
 
 #endif // RAINDROP_SENSOR_HPP
