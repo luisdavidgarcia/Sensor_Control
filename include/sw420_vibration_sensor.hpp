57212d12c0612a7ad9f78522526322803aa96d0a
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-10-28 12:14:53 -0700

    Added sw420 header

--- include/sw420_vibration_sensor.hpp
+++ include/sw420_vibration_sensor.hpp
@@ -1,10 +1,18 @@
 #ifdef SW420_VIBRATION_SENSOR_HPP
 #define SW420_VIBRATION_SENSOR_HPP
 
+#include <wiringPi.h>
+
 class SW420_Vibration_Sensor
 {
 public:
+  SW420_Vibration_Sensor(int pinNumber) : pinNumber_(pinNumber)
+  {
+    pinMode(pinNumber_, INPUT);
+  }
+  bool isThereVibration();
 private:
+  int pinNumber_;
 };
 
 #endif // SW420_VIBRATION_SENSOR_HPP
