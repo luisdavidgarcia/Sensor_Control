1b2ae46925815298d5d9f293fed5baf6f507c370
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-10-28 12:15:07 -0700

    Added sw520d tilt header

--- include/sw520d_tilt_sensor.hpp
+++ include/sw520d_tilt_sensor.hpp
@@ -1,10 +1,18 @@
 #ifndef SW520D_TILT_SENSOR_HPP
 #define SW520D_TILT_SENSOR_HPP
 
+#include <wiringPi.h>
+
 class SW520D_Tilt_Sensor
 {
 public:
+  SW520D_TiltSensor(int pinNumber) : pinNumber_(pinNumber)
+  {
+    pinMode(pinNumber_, INPUT);
+  }
+  bool isThereTilt();
 private:
+  int pinNumber_;
 };
 
 #endif // SW520D_TILT_SENSOR_HPP
