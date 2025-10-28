5a49f666cfa42b8400ba5dc63e059c6b80304e80
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-10-28 12:15:47 -0700

    Ultrasonic sensor implementation

--- src/hcsr04_ultrasonic_sensor.cpp
+++ src/hcsr04_ultrasonic_sensor.cpp
@@ -1,1 +1,32 @@
 #include "hcsr04_ultrasonic_sensor.hpp"
+
+float HCSR04_Ultrasonic_Sensor::getPulseDuration_s()
+{
+  float pulse_start{};
+  while (digitalRead(echoPinNumber_) == 0)
+  {
+    pulseStart = // get current time
+  }
+
+  float pulse_end{};
+  while(digitalRead(echoPinNumber_) == 1)
+  {
+    pulseEnd = // get current time
+  }
+  
+  return pulseEnd - pulseStart;
+}
+
+float HCSR04_Ultrasonic_Sensor::getDistance_m()
+{
+  digitalWrite(triggerPinNumber_, LOW);
+  // Delay 2 microseconds
+  digitalWrite(triggerPinNumber_, HIGH);
+  // Delay 10 microseconds
+  digitalWrite(triggerPinNumber_, LOW);
+
+  float duration = getPulseDuration_s();
+
+  // Speed of sound in m/s divided in half because of roundtrip-time
+  float distance = (duration * 0.343) / 2;
+}
