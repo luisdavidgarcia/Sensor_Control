5f1ca1c974f2daae64fff6a2e4ee060162f8e2e3
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-11-04 11:57:42 -0800

    Added controller to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
 #include <wiringPi.h>
 
+#include "hcsr04_ultrasonic_sensor.hpp"
+#include "raindrop_sensor.hpp"
+#include "sg90_servo_motor.hpp"
+#include "sw420_vibration_sensor.hpp"
+#include "sg90_servor_motor.hpp"
+#include <iostream>
+
 void controller() 
 {
   wiringPiSetupGpio();
   pwmSetMode(PWM_MODE_MS);
   pwmSetClock(384);
   pwmSetRange(1000);
+
+  SW420_Vibration_Sensor vibration_module(5);
+
+  while (!vibration_module.isThereVibration()) {
+    std::cout << "no vibration yet\n"; 
+  }
 }
 
 int main()
 {
-    return 0;
+  controller(); 
+  return 0;
 }
