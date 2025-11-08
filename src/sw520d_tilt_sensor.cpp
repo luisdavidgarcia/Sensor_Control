1a4dbbfa485ff82bee8905bffe28ba9f5bdc6e63
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-11-08 13:35:35 -0800

    Updated logic

--- src/sw520d_tilt_sensor.cpp
+++ src/sw520d_tilt_sensor.cpp
@@ -1,6 +1,7 @@
 #include "sw520d_tilt_sensor.hpp"
+#include <iostream>
 
 bool SW520D_Tilt_Sensor::isThereTilt()
 {
-  return digitalRead(pinNumber_) != 0;
+  return digitalRead(pinNumber_) == LOW;
 }
